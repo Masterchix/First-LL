81c78192567fef32d996cf0c9c749603d51d732f
Masterchix <139686771+Masterchix@users.noreply.github.com>
2024-05-07 22:54:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("Hello world");
+        System.out.println("My name is S");
+        System.out.print((15 + 10) * 3);
+        System.out.println(" -> ");
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("   ***   ");
+        System.out.println("   ***   ");
+  }
+}
